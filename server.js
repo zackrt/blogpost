9e2539399b5373e9604fb99293512475c5221f26
Zack Toth <zacharyrtoth@gmail.com>
2017-12-21 21:51:32 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+// create express, morgon logging const
+const express = require('express');
+
+const morgan = require('morgan');
+
+const blogPostRouter = require('./blogPostRouter');
+
+const app = express();
+
+app.use(morgan('common'));
+
+app.use('./blog-posts',blogPostRouter);
+
+app.listen(process.env.PORT || 8080, () => {
+    console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+  });
